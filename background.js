7f27b2adb6d2ccb34adcb0579bcc52f14590b97b
Víctor Fernández <vfrico@gmail.com>
2018-06-27 00:16:39 +0200

    Initial commit

---
+++ background.js
@@ -0,0 +1,19 @@
+var theme = {
+    colors: {
+        accentcolor: "#31363b",
+        textcolor: "#ffffff",
+
+        toolbar: "#f1f3f7",
+        tab_line: "#2478c8",
+        toolbar_text: "#31363b",
+        toolbar_bottom_separator: "#a7c6e3",
+
+        toolbar_field: "#e9ecf1",
+        toolbar_field_text: "#31363b",
+
+        popup: "#31363b",
+        popup_text: "#ffffff"
+    }
+};
+
+browser.theme.update(theme);
